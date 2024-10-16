64effd51f2b39abbb3f4550489bea3a5cb366634
Ryan Azim <ryanazimr50@gmail.com>
2024-10-17 04:56:35 +0600

    updated leftsidebar and added clerk authentication , home logo

---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link';
+import { IoMdPaw } from "react-icons/io";
+import { HiHome } from 'react-icons/hi';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from '@clerk/nextjs';
+// import MiniProfile from './MiniProfile';
+export default function Sidebar() {
+  return (
+    <div className='flex flex-col p-3 justify-between h-screen items-center'>
+      <div className='flex flex-col gap-4 p-3'>
+        <Link href='/'>
+          <IoMdPaw className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
+        </Link>
+        <Link
+          href='/'
+          className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
+        >
+          <HiHome className='w-7 h-7' />
+          <span className='font-bold hidden xl:inline'>Home</span>
+        </Link>
+        <button className='bg-red-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
+          <SignedIn>
+            <SignOutButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </button>
+      </div>
+ 
+    </div>
+  );
+}
