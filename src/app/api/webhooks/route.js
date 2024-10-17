ea4e165f47ea18715d30cbece4e5985ec3044918
Ryan Azim <ryanazimr50@gmail.com>
2024-10-18 05:06:07 +0600

    TO DEPLOY

--- src/app/api/webhooks/route.js
+++
@@ -1,5 +0,0 @@
-import { Webhook } from 'svix';
-import { headers } from 'next/headers';
-import { clerkClient } from '@clerk/nextjs/server';
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
-
